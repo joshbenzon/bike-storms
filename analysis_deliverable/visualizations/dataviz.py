d828759b3cc312000ec194165cdbc223d4c3e879
ceboop <84917913+ceboop@users.noreply.github.com>
2023-05-14 09:35:03 -0700

    plots

--- analysis_deliverable/visualizations/dataviz.py
+++ analysis_deliverable/visualizations/dataviz.py
@@ -1,17 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
+dataset = pd.read_csv('../../data/final_data.csv')
+#there was one egregious outlier
+dataset = dataset[dataset['avgBikeDuration'] > 50]
 
-dataset = pd.read_csv('../data/final_data.csv')
-
-#LASTLY SCATTER PLOT CHART FOR BANKNOTES
 temperature = dataset['avgTemp']
 bike_length = dataset['avgBikeDuration']
 taxi_length = dataset['avgTaxiDuration']
 
-#taken from matplotlib site
-fig, ax = plt.subplots()
-ax.plot(temperature, bike_length)
-ax.plot(temperature, taxi_length)
-ax.set_title('Scatter Plot of Temperature vs. Average Bike Duration vs. Average Taxi Duration')
+sns.scatterplot(x=temperature, y=bike_length, label='bike', cmap='viridis')
+sns.scatterplot(x=temperature, y=taxi_length, label='taxi', cmap='viridis')
+
+
+plt.xlabel('Average Temperature (F)')
+plt.ylabel('Average Daily Trip Duration (s)')
+plt.title('Average Daily Temperature vs. Bike & Taxi Trip Duration')
+
 plt.show()
